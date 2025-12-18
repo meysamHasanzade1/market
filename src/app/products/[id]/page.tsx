569bfd0b3d3ffface1ecee06cb036cbbecab3cc7
meysam <meysam.hasanzade0101@gmail.com>
2025-12-19 01:04:43 +0330

    add commit

--- src/app/products/[id]/page.tsx
+++ src/app/products/[id]/page.tsx
@@ -1,6 +1,7 @@
 import AddToCartButton from "@/components/AddToCartButton";
 import { notFound } from "next/navigation";
-import React from "react";
+import Image from "next/image";
+
 interface Product {
   id: number;
   title: string;
@@ -10,55 +11,72 @@ interface Product {
   images: string[];
 }
 
-async function gteProduct(id: string): Promise<Product | null> {
+async function getProduct(id: string): Promise<Product | null> {
   try {
     const res = await fetch(`https://dummyjson.com/products/${id}`, {
       cache: "no-store",
     });
+
     if (!res.ok) return null;
 
     return res.json();
   } catch (error) {
-    console.error("error fetching product:", error);
+    console.error("Error fetching product:", error);
     return null;
   }
 }
-async function ProductDetailsPage({ params }: { params: { id: string } }) {
-  const product = await gteProduct(params.id);
+
+type PageProps = {
+  params: Promise<{ id: string }>;
+};
+
+export default async function ProductDetailsPage({ params }: PageProps) {
+  const { id } = await params;
+
+  const product = await getProduct(id);
+
   if (!product) {
-    return notFound();
+    notFound();
   }
+
   return (
     <div className="max-w-4xl mx-auto p-6">
       <div className="grid md:grid-cols-2 gap-8">
+        {/* Images */}
         <div>
-          <img
+          <Image
             src={product.thumbnail}
             alt={product.title}
+            width={600}
+            height={400}
             className="w-full h-96 object-cover rounded-lg shadow"
           />
-          <div className="gird grid-cols-4 gap-2 mt-4">
+
+          <div className="grid grid-cols-4 gap-2 mt-4">
             {product.images.map((img, index) => (
-              <img
+              <Image
                 key={index}
                 src={img}
-                alt={`Image  ${index}`}
+                alt={`Image ${index + 1}`}
+                width={100}
+                height={80}
                 className="w-full h-20 object-cover rounded-md border"
               />
             ))}
           </div>
         </div>
+
+        {/* Details */}
         <div>
           <h1 className="text-3xl font-bold mb-4">{product.title}</h1>
           <p className="text-gray-700 mb-4">{product.description}</p>
           <p className="text-2xl font-semibold text-green-600 mb-6">
             ${product.price}
           </p>
+
           <AddToCartButton product={product} />
         </div>
       </div>
     </div>
   );
 }
-
-export default ProductDetailsPage;
